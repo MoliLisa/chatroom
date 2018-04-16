24337345e759eae92cd7b96c765605f47b254c29
lisalizhi <everythinggoesaway@hotmail.com>
2018-04-16 14:54:23 -0400

    test commit

---
+++ Login/index.php
@@ -0,0 +1,193 @@
+<?php
+	ob_start();
+	session_start();
+	require_once 'dbconnect.php';
+	
+	// it will never let you open index(login) page if session is set
+	if ( isset($_SESSION['user'])!="" ) {
+		header("Location: home.php");
+		exit;
+	}
+	
+	$error = false;
+	
+	if( isset($_POST['btn-login']) ) {	
+		
+		// prevent sql injections/ clear user invalid inputs
+		$email = trim($_POST['email']);
+		$email = strip_tags($email);
+		$email = htmlspecialchars($email);
+		
+		$pass = trim($_POST['pass']);
+		$pass = strip_tags($pass);
+		$pass = htmlspecialchars($pass);
+		// prevent sql injections / clear user invalid inputs
+		
+		if(empty($email)){
+			$error = true;
+			$emailError = "Please enter your email address.";
+		} else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+			$error = true;
+			$emailError = "Please enter valid email address.";
+		}
+		
+		if(empty($pass)){
+			$error = true;
+			$passError = "Please enter your password.";
+		}
+		
+		// if there's no error, continue to login
+		if (!$error) {
+			
+			$password = hash('sha256', $pass); // password hashing using SHA256
+		
+			$res=mysql_query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
+			$row=mysql_fetch_array($res);
+			$count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
+			
+			if( $count == 1 && $row['userPass']==$password ) {
+				$_SESSION['user'] = $row['userId'];
+				header("Location: home.php");
+			} else {
+				$errMSG = "Incorrect Credentials, Try again...";
+			}
+				
+		}
+		
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="vendor/select2/select2.min.js"></script>
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<script src="js/main.js"></script>
+<title>Login & Registration System</title>
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css"  />
+</head>
+<body>
+
+<div class="container">
+
+	<div id="login-form">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+    
+    	<div class="col-md-12">
+        
+        	<div class="form-group">
+            	<h2 class="">Sign In.</h2>
+            </div>
+        
+        	<div class="form-group">
+            	<hr />
+            </div>
+            
+            <?php
+			if ( isset($errMSG) ) {
+				
+				?>
+				<div class="form-group">
+            	<div class="alert alert-danger">
+				<span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                </div>
+            	</div>
+                <?php
+			}
+			?>
+            
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+            	<input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $email; ?>" maxlength="40" />
+                </div>
+                <span class="text-danger"><?php echo $emailError; ?></span>
+            </div>
+            
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+            	<input type="password" name="pass" class="form-control" placeholder="Your Password" maxlength="15" />
+                </div>
+                <span class="text-danger"><?php echo $passError; ?></span>
+            </div>
+            
+            <div class="form-group">
+            	<hr />
+            </div>
+            
+            <div class="form-group">
+            	<button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
+            </div>
+            
+            <div class="form-group">
+            	<hr />
+            </div>
+            
+            <div class="form-group">
+            	<a href="register.php">Sign Up Here...</a>
+            </div>
+        
+        </div>
+   
+    </form>
+    </div>	
+
+</div>
+
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+			<div class="wrap-login100 p-t-30 p-b-50">
+				<span class="login100-form-title p-b-41">
+					Account Login
+				</span>
+				<form class="login100-form validate-form p-b-33 p-t-5">
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" type="text" name="username" placeholder="User name">
+						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-32">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+
+
+</body>
+</html>
+<?php ob_end_flush(); ?>
